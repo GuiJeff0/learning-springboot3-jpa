daf3058b32b0552ddd1e75c923331b0b14ce0fd7
creedx <guilhermejeffofc@gmail.com>
2024-06-01 18:32:30 -0300

    OrderStatus enum

---
+++ src/main/java/com/learningweb/webservices/services/CategoryService.java
@@ -0,0 +1,30 @@
+package com.learningweb.webservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learningweb.webservices.entities.Category;
+import com.learningweb.webservices.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
