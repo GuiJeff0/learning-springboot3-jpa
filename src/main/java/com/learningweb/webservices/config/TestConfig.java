0645940106d97106044a4c909893903292d5203a
creedx <guilhermejeffofc@gmail.com>
2024-06-02 09:07:05 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/learningweb/webservices/config/TestConfig.java
+++ src/main/java/com/learningweb/webservices/config/TestConfig.java
@@ -60,7 +60,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
-		
+
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
--- src/main/java/com/learningweb/webservices/entities/Category.java
+++ src/main/java/com/learningweb/webservices/entities/Category.java
@@ -75,11 +75,5 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-
-	
-	
-	
 	
 }
