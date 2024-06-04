d942994ae2f1d25ee46be00353f3cab69202c208
creedx <guilhermejeffofc@gmail.com>
2024-06-04 14:10:07 -0300

    Payment, one-to-one association

--- src/main/java/com/learningweb/webservices/config/TestConfig.java
+++ src/main/java/com/learningweb/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.learningweb.webservices.entities.Category;
 import com.learningweb.webservices.entities.Order;
 import com.learningweb.webservices.entities.OrderItem;
+import com.learningweb.webservices.entities.Payment;
 import com.learningweb.webservices.entities.Product;
 import com.learningweb.webservices.entities.User;
 import com.learningweb.webservices.entities.enums.OrderStatus;
@@ -87,5 +88,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2, oi3, oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
