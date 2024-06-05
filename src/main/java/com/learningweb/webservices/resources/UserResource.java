ca4ca8ef892d14e94fdd4c4258b30c4d109e1425
creedx <guilhermejeffofc@gmail.com>
2024-06-05 15:43:46 -0300

    insert User

--- src/main/java/com/learningweb/webservices/resources/UserResource.java
+++ src/main/java/com/learningweb/webservices/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.learningweb.webservices.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.learningweb.webservices.entities.User;
 import com.learningweb.webservices.services.UserService;
@@ -33,5 +37,13 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	@PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User createdUser = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        		.path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(createdUser);
+    }
 	
 }
