af903289ef4fe9c0005487c39f2b8b3519bad1c6
creedx <guilhermejeffofc@gmail.com>
2024-06-06 13:40:25 -0300

    Exception handling - delete

--- src/main/java/com/learningweb/webservices/services/UserService.java
+++ src/main/java/com/learningweb/webservices/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.learningweb.webservices.entities.User;
 import com.learningweb.webservices.repositories.UserRepository;
+import com.learningweb.webservices.services.exceptions.DatabaseException;
 import com.learningweb.webservices.services.exceptions.ResourceNotFoundException;
 
 
@@ -33,7 +36,14 @@ public class UserService {
     }
 	
 	public void delete(Long id) {
-        repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+        
     }
 	
 	public User update(Long id, User obj) {
