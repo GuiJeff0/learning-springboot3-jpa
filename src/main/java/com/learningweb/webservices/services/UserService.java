2819ef8ffab60852667de378b985608ad16feebd
creedx <guilhermejeffofc@gmail.com>
2024-06-06 13:47:05 -0300

    Exception handling - update

--- src/main/java/com/learningweb/webservices/services/UserService.java
+++ src/main/java/com/learningweb/webservices/services/UserService.java
@@ -13,6 +13,8 @@ import com.learningweb.webservices.repositories.UserRepository;
 import com.learningweb.webservices.services.exceptions.DatabaseException;
 import com.learningweb.webservices.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -47,9 +49,14 @@ public class UserService {
     }
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-        return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+	        return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		} 
+		
     }
 	
 	private void updateData(User entity, User obj) {
